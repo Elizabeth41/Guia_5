d7dc424a9e4b3e3eed42f32fb33d4eaef1ffbc5f
Elizabeth41 <42724655+Elizabeth41@users.noreply.github.com>
2018-11-30 21:59:15 -0600

    Add files via upload

---
+++ src/main/java/org/dev4u/hv/guia5_ejemplo/PrincipalActivity.java
@@ -0,0 +1,42 @@
+package org.dev4u.hv.guia5_ejemplo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    Button btnEjemplos,btnEjercicios;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        btnEjemplos     = (Button) findViewById(R.id.btnEjemplos);
+        btnEjercicios   = (Button) findViewById(R.id.btnEjercicios);
+
+
+        btnEjemplos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        PrincipalActivity.this,
+                        EjemploActivity.class
+                ));
+            }
+        });
+        btnEjercicios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(
+                        PrincipalActivity.this,
+                        EjercicioActivity.class
+                ));
+            }
+        });
+
+    }
+}
